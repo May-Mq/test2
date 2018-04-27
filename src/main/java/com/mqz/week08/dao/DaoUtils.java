a15613c14245c8b446527046f1d2a56cf5f07828
zmq <1095981251@qq.com>
2018-04-27 11:27:34 +0800

    week08

---
+++ src/main/java/com/mqz/week08/dao/DaoUtils.java
@@ -0,0 +1,24 @@
+package com.mqz.week08.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DaoUtils {
+	public static Connection getConn() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			conn=DriverManager.getConnection("jdbc:mysql:localhost:3306/shopdb","root","123456");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
